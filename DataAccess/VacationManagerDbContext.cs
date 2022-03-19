615a9ddb6ad9e26fad880bff77beb4905e32de85
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2022-03-19 21:19:21 +0200

    Create VacationManagerDbContext class

---
+++ DataAccess/VacationManagerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataAccess
+{
+    public class VacationManagerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Vacation> Vacations { get; set; }
+
+        public VacationManagerDbContext(DbContextOptions<VacationManagerDbContext> options) : base(options) { }
+    }
+}
