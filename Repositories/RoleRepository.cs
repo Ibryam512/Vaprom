ba27db3c89b240a8ef7f64be97ca5e2cad1b0a94
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2022-03-20 14:38:01 +0200

    Add ITeamRepository and TeamRepository

--- Repositories/RoleRepository.cs
+++ Repositories/RoleRepository.cs
@@ -2,10 +2,7 @@
 using Models;
 using Repositories.Interfaces;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repositories
 {
---
+++ Repositories/TeamRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess;
+using Models;
+using Repositories.Interfaces;
+using System;
+using System.Linq;
+
+namespace Repositories
+{
+    public class TeamRepository : ITeamRepository
+    {
+        private readonly VacationManagerDbContext _context;
+
+        public TeamRepository(VacationManagerDbContext context)
+        {
+            this._context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IQueryable<Team> GetTeams() => this._context.Teams;
+
+        public Team GetTeam(string id) => this._context.Teams.Find(id);
+
+        public void AddTeam(Team team)
+        {
+            this._context.Teams.Add(team);
+            this._context.SaveChanges();
+        }
+
+        public void EditTeam(Team team)
+        {
+            this._context.Teams.Update(team);
+            this._context.SaveChanges();
+        }
+
+        public void DeleteTeam(Team team)
+        {
+            this._context.Teams.Remove(team);
+            this._context.SaveChanges();
+        }
+    }
+}
