45274b27edb0536a5c3ea55cb3864f83f6e39343
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2022-03-20 14:46:11 +0200

    Add IVacationRepository and VacationRepository

---
+++ Repositories/Interfaces/IVacationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System.Linq;
+
+namespace Repositories.Interfaces
+{
+    public interface IVacationRepository
+    {
+        IQueryable<Vacation> GetVacations();
+        Vacation GetVacation(string id);
+        void AddVacation(Vacation vacation);
+        void EditVacation(Vacation vacation);
+        void DeleteVacation(Vacation vacation);
+    }
+}
---
+++ Repositories/VacationRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess;
+using Models;
+using Repositories.Interfaces;
+using System;
+using System.Linq;
+
+namespace Repositories
+{
+    public class VacationRepository : IVacationRepository
+    {
+        private readonly VacationManagerDbContext _context;
+
+        public VacationRepository(VacationManagerDbContext context)
+        {
+            this._context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IQueryable<Vacation> GetVacations() => this._context.Vacations;
+
+        public Vacation GetVacation(string name) => this._context.Vacations.Find(name);
+
+        public void AddVacation(Vacation vacation)
+        {
+            this._context.Vacations.Add(vacation);
+            this._context.SaveChanges();
+        }
+
+        public void EditVacation(Vacation vacation)
+        {
+            this._context.Vacations.Update(vacation);
+            this._context.SaveChanges();
+        }
+
+        public void DeleteVacation(Vacation vacation)
+        {
+            this._context.Vacations.Remove(vacation);
+            this._context.SaveChanges();
+        }
+    }
+}
