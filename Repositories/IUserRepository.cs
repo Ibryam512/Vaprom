bfafa9857262e0a33c5c306242d0eabb8f2d32fd
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2022-03-20 12:30:43 +0200

    Add UserRepository and IUserRepository with basic CRUD operations

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IUserRepository
+    {
+        IQueryable<User> GetUsers();
+        User GetUser(string id);
+        void AddUser(User user);
+        void EditUser(User user);
+        void DeleteUser(User user);
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccess;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly VacationManagerDbContext _context;
+
+        public UserRepository(VacationManagerDbContext context)
+        {
+            this._context = context;
+        }
+
+        public IQueryable<User> GetUsers() => this._context.Users;
+
+        public User GetUser(string id) => this._context.Users.Find(id);
+
+        public void AddUser(User user)
+        {
+            this._context.Add(user);
+            this._context.SaveChanges();
+        }
+
+        public void EditUser(User user)
+        {
+            this._context.Update(user);
+            this._context.SaveChanges();
+        }
+
+        public void DeleteUser(User user)
+        {
+            this._context.Remove(user);
+            this._context.SaveChanges();
+        }
+    }
+}
