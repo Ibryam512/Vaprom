ce81d11b398a75d4c6251b18d0f42bd0c794a2e1
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2022-03-20 14:42:11 +0200

    Add IProjectRepository and ProjectRepository

---
+++ Repositories/Interfaces/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System.Linq;
+
+namespace Repositories.Interfaces
+{
+    public interface IProjectRepository
+    {
+        IQueryable<Project> GetProjects();
+        Project GetProject(string id);
+        void AddProject(Project project);
+        void EditProject(Project project);
+        void DeleteProject(Project project);
+    }
+}
---
+++ Repositories/ProjectRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess;
+using Models;
+using Repositories.Interfaces;
+using System;
+using System.Linq;
+
+namespace Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly VacationManagerDbContext _context;
+
+        public ProjectRepository(VacationManagerDbContext context)
+        {
+            this._context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IQueryable<Project> GetProjects() => this._context.Projects;
+
+        public Project GetProject(string id) => this._context.Projects.Find(id);
+
+        public void AddProject(Project project)
+        {
+            this._context.Projects.Add(project);
+            this._context.SaveChanges();
+        }
+
+        public void EditProject(Project project)
+        {
+            this._context.Projects.Update(project);
+            this._context.SaveChanges();
+        }
+
+        public void DeleteProject(Project project)
+        {
+            this._context.Projects.Remove(project);
+            this._context.SaveChanges();
+        }
+    }
+}
