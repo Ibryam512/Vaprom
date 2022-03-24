ac91f9c4382c6a7f31b613bd2c13859b47427997
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-03-24 23:14:25 +0200

    Add all ViewModels to current models

---
+++ ViewModels/Input/RoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels
+{
+	public class RoleDTO
+	{
+		[Required(ErrorMessage = "Името на ролята е задължително")]
+		public string Name { get; set; }
+	}
+}
