6313106a6df0289333bc210fc933096a4b59da4a
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-03-30 00:11:42 +0300

    Add create team design

---
+++ Web/Controllers/TeamController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewModels.Input;
+
+namespace Web.Controllers
+{
+	public class TeamController : Controller
+	{
+		[HttpGet]
+		public IActionResult Create()
+		{
+			TeamViewModel model = new TeamViewModel();
+
+			return View(model);
+		}
+	}
+}
