3477e50a5802245825a0205d49c602b71dc2cded
Ibryam512 <bamko2003@gmail.com>
2022-04-01 20:35:03 +0300

    Add IUserService and UserService

---
+++ Web/Services/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+using Models;
+using System.Collections.Generic;
+
+namespace Web.Services.Interfaces
+{
+    public interface IUserService
+    {
+        List<User> GetUsers();
+        User GetUser(string id);
+        void AddUser(User user);
+        void ChangeRole(User user, Role role);
+        void JoinTeam(User user, Team team);
+        void LeaveTeam(User user);
+        void EditUser(User user);
+        void DeleteUser(User user);
+    }
+}
