9e068833ebb3c811f73a158e2f6d746a724eeb7c
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-07 11:32:33 +0300

    Add create role page

---
+++ Web/Controllers/RoleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+	public class RoleController : Controller
+	{
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+	}
+}
