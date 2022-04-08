15b8643472fc0d8153e130cff61ecced42aad960
Ангел Момов <angelpikk@gmail.com>
2022-04-08 14:21:17 +0300

    n

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -40,6 +40,7 @@ namespace VacationManager.Web
 
             //Services
             services.AddScoped<ITeamService, TeamService>();
+            services.AddScoped<IRoleService, RoleService>();
             services.AddScoped<IVacationDocumentService, VacationDocumentService>();
 
             services.AddDbContext<VacationManagerDbContext>(options =>
