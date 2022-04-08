4936cfc65e1362985d5652e8198e1f30f2948ba3
Valeria Stamenova <valeria.stamenova3@gmail.com>
2022-04-09 01:43:19 +0300

    Add project search

---
+++ Models/SearchModel/ProjectSearch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.SearchModel
+{
+	public class ProjectSearch
+	{
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public List<Project> Results { get; set; }
+	}
+}
